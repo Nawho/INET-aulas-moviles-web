8606a5b7de12b4c8ff8182ca2f54fa2a309c633e
Nawho <Nawho>
2023-09-19 15:56:37 -0300

    first commit, refactoring page

---
+++ resources/views/bots/chat.blade.php
@@ -0,0 +1,27 @@
+<script>
+    // Example ussage of the fullscreen widget
+    const welcomeMessage = "¡Hola! Soy Botinet, fui creado para asistirte en tus dudas respecto a las aulas virtuales.";
+    const chatServer = "/api/botman";
+ 
+    // Create a new observer instance:
+    const observer = new MutationObserver(function() {
+        if (document.getElementById('botmanChatRoot')) {
+            // You must wait until the react component is inserted on the body!
+            window.BotmanInstance.chatServer = chatServer;
+            window.chatInstance.sayAsBot(welcomeMessage);
+            disconectObserver();
+        }
+    });
+
+    // Set configuration object:
+    const config = { childList: true };
+    
+    // Start the observer
+    observer.observe(document.body, config);
+
+    function disconectObserver() {
+        observer.disconnect();
+    }
+
+</script>
+@vite(['resources/js/botman/fullscreen.js', 'resources/css/chat.css'])
---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>INET - Aulas móviles</title>
+
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="{{ asset( 'css/app.css') }}" rel="stylesheet" type="text/css">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <header class="app-header">
+            INET - Información de aulas móviles
+            <nav>
+
+            </nav>
+        </header>
+
+        @include('bots.chat')
+    </body>
+</html>
